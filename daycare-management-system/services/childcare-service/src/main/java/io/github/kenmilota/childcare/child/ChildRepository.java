15b9103823ddf4e2d2442c41a96e04ec526e39f3
Kenneth Milota <kenm661@gmail.com>
2026-07-16 16:58:15 -0400

    Delete daycare-management-system directory

--- daycare-management-system/services/childcare-service/src/main/java/io/github/kenmilota/childcare/child/ChildRepository.java
+++
@@ -1,9 +0,0 @@
-package io.github.kenmilota.childcare.child;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface ChildRepository extends JpaRepository<Child, UUID> {
-}
